a332252ce31f370e9456849ac5b0d865cb3f5a60
lizakuznetsova <kuznetsova.ea@phistech.edu>
2021-04-15 10:19:37 +0300

    прикрепляю задачку номер два

---
+++ TEST_1/2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+
+
+
+int sum(int n) {
+    int s = 0;
+    int *p;
+    int *a = new int[n];
+    p = a;
+
+
+    while (a != p + n) {
+        std::cin >> *a;
+        s += *a;
+        ++a;
+    }
+    return s;
+}
+
+int main(){
+    int n;
+    std::cin >> n;
+    std::cout << std::fixed << std::setprecision(10);
+    std::cout << float(sum(n) / n);
+
+
+    return 0;
+}
